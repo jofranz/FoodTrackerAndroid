e270d93acab32764692107023c52b027be715ac3
Normen <normen.krug@web.de>
2017-10-16 17:16:19 +0200

    added working MapView and LocationPermisson check for android 6.0 and higher

--- app/src/main/java/app/foodtracker/de/foodtracker/MainActivity.java
+++ app/src/main/java/app/foodtracker/de/foodtracker/MainActivity.java
@@ -1,8 +1,14 @@
 package app.foodtracker.de.foodtracker;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -48,5 +54,66 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asy
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_permission)
+                        .setMessage(R.string.text_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                } else {
+                    //TODO
+                    // permission denied, boo! Disable the
+
+
+                }
+                return;
+            }
+
+        }
     }
 }
--- app/src/main/java/app/foodtracker/de/foodtracker/MapFragment.java
+++ app/src/main/java/app/foodtracker/de/foodtracker/MapFragment.java
@@ -1,11 +1,90 @@
 package app.foodtracker.de.foodtracker;
 
 import android.app.Fragment;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by normen on 16.10.17.
  */
 
 public class MapFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap googleMap;
+    private Button mapAdd;
+    private LocationManager locationManager;
+    private String provider;
+    private LatLng currentLocation;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.map_fragment, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+        mapAdd = (Button) rootView.findViewById(R.id.addButton);
+        mapAdd.setEnabled(false);
+        mapAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((MainActivity)getActivity()).checkLocationPermission()) {
+                    Location location = locationManager.getLastKnownLocation(provider);
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+
+                    currentLocation = new LatLng(lat, lng);
+                    googleMap.addMarker(new MarkerOptions().position(currentLocation).title("You are her")
+                            .snippet("This is a snippet:" + currentLocation.toString()));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                }
+            }
+        });
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                if(((MainActivity)getActivity()).checkLocationPermission()){
+                    googleMap.setMyLocationEnabled(true);
+                }
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+        return rootView;
+    }
+
 
 }
