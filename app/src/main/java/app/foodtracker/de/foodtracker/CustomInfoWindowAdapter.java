3c94db15ca591b0ea3128efb2de908fb983a4468
Normen <normen.krug@web.de>
2017-11-02 18:19:51 +0100

    Fixed app crash caused by getLastKnowPosition returns null Added own data representation for InfoWindow Added custom Infowindow

--- app/src/main/java/app/foodtracker/de/foodtracker/CustomInfoWindowAdapter.java
+++ app/src/main/java/app/foodtracker/de/foodtracker/CustomInfoWindowAdapter.java
@@ -1,11 +1,19 @@
 package app.foodtracker.de.foodtracker;
 
 import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Gallery;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+
+import app.foodtracker.de.foodtracker.Model.MarkerRepresentation;
 
 /**
  * Created by normen on 17.10.17.
@@ -13,22 +21,47 @@ import com.google.android.gms.maps.model.Marker;
 
 //### work in progress ###
 
-public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
 
     private final View mWindow;
-    private View layoutInflater;
+    private View v;
+    private LayoutInflater layoutInflater;
 
     CustomInfoWindowAdapter(Activity activity) {
         mWindow = activity.getLayoutInflater().inflate(R.layout.costum_info_window, null);
-    }
+        layoutInflater = activity.getLayoutInflater();
 
+    }
     @Override
     public View getInfoWindow(Marker marker) {
+
+
+
         return null;
     }
 
     @Override
     public View getInfoContents(Marker marker) {
-        return null;
+
+        v = layoutInflater.inflate(R.layout.costum_info_window,null);
+
+        //get the postion of the marker
+        LatLng latLng = marker.getPosition();
+
+        TextView infoMeal = v.findViewById(R.id.infoMeal);
+        TextView infoTime = v.findViewById(R.id.infoTime);
+
+        MarkerRepresentation markerRe = (MarkerRepresentation) marker.getTag();
+
+        infoMeal.setText(markerRe.getMeal());
+        infoTime.setText(markerRe.getTime().getTime().toString());
+
+
+
+        return v;
+    }
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Log.d("bonobo","bonobo");
     }
 }
--- app/src/main/java/app/foodtracker/de/foodtracker/MapFragment.java
+++ app/src/main/java/app/foodtracker/de/foodtracker/MapFragment.java
@@ -1,15 +1,23 @@
 package app.foodtracker.de.foodtracker;
 
+
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,13 +27,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import java.security.Provider;
+
+import app.foodtracker.de.foodtracker.Model.MarkerRepresentation;
 
 /**
  * Created by normen on 16.10.17.
  */
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements LocationListener{
 
     MapView mMapView;
     private GoogleMap googleMap;
@@ -33,6 +47,9 @@ public class MapFragment extends Fragment {
     private LocationManager locationManager;
     private String provider;
     private LatLng currentLocation;
+    private double lat;
+    private double lng;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,23 +58,35 @@ public class MapFragment extends Fragment {
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
+
         mMapView.onResume(); // needed to get the map to display immediately
         mapAdd = (Button) rootView.findViewById(R.id.addButton);
+        Criteria crit = new Criteria();
+        locationManager = (LocationManager)((MainActivity)getActivity()).getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(crit,true);
+
+
+
+
+
+
         mapAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(((MainActivity)getActivity()).checkLocationPermission()) {
-                    locationManager = (LocationManager)((MainActivity)getActivity()).getSystemService(Context.LOCATION_SERVICE);
 
-                    provider = locationManager.getBestProvider(new Criteria(), false);
 
-                    Location location = locationManager.getLastKnownLocation(provider);
-                    double lat = location.getLatitude();
-                    double lng = location.getLongitude();
+
+
+
+
+                    MarkerRepresentation markerRepresentation = new MarkerRepresentation();
 
                     currentLocation = new LatLng(lat, lng);
+
                     googleMap.addMarker(new MarkerOptions().position(currentLocation).title("You are her")
-                            .snippet("This is a snippet:" + currentLocation.toString()));
+                            .snippet("This is a snippet:" + currentLocation.toString())).setTag(markerRepresentation);
+                    googleMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(getActivity()));
                     googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
                 }
             }
@@ -79,16 +108,75 @@ public class MapFragment extends Fragment {
                 if(((MainActivity)getActivity()).checkLocationPermission()){
                     googleMap.setMyLocationEnabled(true);
                 }
+
+
                 // For dropping a marker at a point on the Map
                 LatLng sydney = new LatLng(-34, 151);
                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
+                googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                    @Override
+                    public void onInfoWindowClick(Marker marker) {
+                        Toast.makeText(getActivity(), "Info window clicked",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
-
         return rootView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(((MainActivity)getActivity()).checkLocationPermission()) {
+
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            if (location == null) {
+                locationManager.requestLocationUpdates(provider, 0, 0, this);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("onLocationChanged","bonobo");
+        if(((MainActivity)getActivity()).checkLocationPermission()){
+             lat = location.getLatitude();
+             lng = location.getLongitude();
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(this);
+    }
+
+
+
+
 }
