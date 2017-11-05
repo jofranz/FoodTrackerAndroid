e55379ec1eb78ed609b494d44a169e4b99e39310
Normen <normen.krug@web.de>
2017-11-05 19:01:04 +0100

    Adapt the layout to material design

---
+++ app/src/main/java/app/foodtracker/de/foodtracker/AddFragment.java
@@ -0,0 +1,73 @@
+package app.foodtracker.de.foodtracker;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
+
+
+/**
+ * Created by jfranz on 02.11.2017.
+ */
+
+public class AddFragment extends Fragment {
+
+    private ImageView imageView;
+    private EditText foodName, addressEdit, snippet;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.add_fragment, container, false);
+
+
+
+        imageView = rootView.findViewById(R.id.picturePreview);
+        addressEdit = rootView.findViewById(R.id.addressEdit);
+        foodName = rootView.findViewById(R.id.foodName);
+        snippet = rootView.findViewById(R.id.snippet);
+
+
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+
+            }
+        });
+
+        return rootView;
+    }
+
+
+
+
+}
